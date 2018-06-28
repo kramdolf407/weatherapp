8aca4fdc27f00065a1de38599547a997743dc6c8
Peter Flodmark <pfpeterflodmark@gmail.com>
2018-06-28 16:30:10 +0200

    yes

--- backend/tests/request_to_backend.py
+++ backend/tests/request_to_backend.py
@@ -6,9 +6,13 @@ import requests
 r = requests.get('http://localhost:9000/api/weather')
 
 if (r.status_code == requests.codes.ok):
+  print("*******")
   print("200 OK")
+  print("*******")
 
 dict1 = json.loads(r.text)
 
 if ('icon' in dict1.keys()):
+  print("*******")
   print("Icon detected..")
+  print("*******")
