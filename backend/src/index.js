554808a7b2ed2dc932bcd6411c3e8ac1dda4c9a6
Peter Flodmark <pfpeterflodmark@gmail.com>
2018-06-29 09:01:04 +0200

    testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '8f15d45b18dd3f64d682903a1aea784a';
+const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Stockholm,se";
 
