ee76abac14a6a29d1be04b9513c68d30e4efd737
Peter Flodmark <pfpeterflodmark@gmail.com>
2018-06-29 11:13:11 +0200

    works

--- backend/tests/request_to_backend.py
+++ backend/tests/request_to_backend.py
@@ -4,20 +4,11 @@ import requests
 r = requests.get('http://localhost:9000/api/weather')
 
 if (r.status_code == requests.codes.ok):
-  print("*******")
-  print("200 OK")
-  print("*******")
-
-
-print("Do we get content??")
-print(r.headers)
+    print("-------")
+    print("Backend site responded with 200 OK")
+    print("-------")
 
 dict1 = json.loads(r.text)
 
 if ('icon' in dict1.keys()):
-  print("*******")
-  print("Icon detected..")
-  print("*******")
-
-
-print("end python script")
+    print("Icon detected..")
